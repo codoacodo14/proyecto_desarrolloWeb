34995b6c39c500d35f87a743ccb93a9d81d78e07
laupadron <laupadron1458@gmail.com>
2023-07-01 15:28:51 -0300

    fix: código url al inglés y url API definitiva

--- library_consult/Administrator/add_delete_book.js
+++ library_consult/Administrator/add_delete_book.js
@@ -1,55 +1,53 @@
+//add books
 document.addEventListener('DOMContentLoaded', function() {
-    // Obtener el formulario de agregar libro por su id
     const bookForm = document.getElementById('book-form');
-
-    // Agregar un evento de envío al formulario
-    bookForm.addEventListener('submit', function(event) {
-        event.preventDefault(); // Evitar que el formulario se envíe automáticamente
+      bookForm.addEventListener('submit', function(event) {
+        event.preventDefault(); 
 
         // Obtener los valores de los campos de entrada
-        const autor = document.getElementById('autor').value;
-        const titulo = document.getElementById('titulo').value;
+        const author = document.getElementById('author').value;
+        const title = document.getElementById('title').value;
         const editorial = document.getElementById('editorial').value;
-        const disponibilidad = document.getElementById('disponibilidad').value
+        const availability = document.getElementById('availability').value
         const area = document.getElementById('area').value;
 
         // Crear objeto del libro
-        const libro = {
-            autor: autor,
-            titulo: titulo,
+        let book = {
+            author: author,
+            title: title,
             editorial: editorial,
-            disponibilidad: disponibilidad,
+            availability: availability,
             area: area
         };
-        // Obtener el token de autorización del Local Storage
+        // Obtenemos el token de autorización del Local Storage
         const token = localStorage.getItem('token');
 
-        // Realizar solicitud POST al endpoint /api/libros
-        fetch('http://127.0.0.1:4000/api/libros', {
+        // Realizamos solicitud POST al endpoint /api/libros
+        fetch('https://laupadron.pythonanywhere.com/api/books', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + token// Obtener token del Local Storage
+                'Authorization': 'Bearer ' + token
             },
-            body: JSON.stringify(libro)
+            body: JSON.stringify(book)
         })
         .then(function(response) {
             if (response.status === 200 || response.status === 201) {
-                // Libro agregado exitosamente
+                
                 return response.json();
             } else {
-                // Error al agregar el libro
+                
                 throw new Error('Error al agregar el libro');
             }
         })
         .then(function(data) {
-            // Mostrar mensaje de éxito
+            
             alert('Libro agregado exitosamente');
-            // Restablecer campos del formulario
+            
             bookForm.reset();
         })
         .catch(function(error) {
-            // Mostrar mensaje de error
+            
             alert('Error: ' + error.message);
         });
     });
@@ -57,59 +55,63 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-
+//get books para la lista con botones de update y eliminar
 document.addEventListener('DOMContentLoaded', function() {
-    var searchForm = document.getElementById('search_form');
-    var resultsContainer = document.getElementById('book-info-container');
-  
+    let searchForm = document.getElementById('search_form');
+    let resultsContainer = document.getElementById('book-info-container');
+    
     searchForm.addEventListener('submit', function(event) {
-      event.preventDefault(); // Evitar que el formulario se envíe automáticamente
+      event.preventDefault(); 
   
-      var libroNombre = document.getElementById('libro-nombre').value;
-      var libroAutor = document.getElementById('libro-autor').value;
+      let bookName = document.getElementById('book-name').value;
+      let bookAuthor = document.getElementById('book-author').value;
   
       // Construir la URL de búsqueda con los parámetros
       var searchParams = new URLSearchParams();
-      if (libroNombre.trim() !== '') {
-        searchParams.append('titulo', libroNombre);
+      if (bookName.trim() !== '') {
+        searchParams.append('title', bookName);
       }
-      if (libroAutor.trim() !== '') {
-        searchParams.append('autor', libroAutor);
+      if (bookAuthor.trim() !== '') {
+        searchParams.append('author', bookAuthor);
       }
-      var searchUrl = 'http://127.0.0.1:4000/api/libros?' + searchParams.toString();
+      let searchUrl = 'https://laupadron.pythonanywhere.com/api/books?' + searchParams.toString();
   
       // Realizar la solicitud GET al servidor para buscar el libro
       fetch(searchUrl)
         .then(function(response) {
           if (response.ok) {
-            return response.json(); // Parsear la respuesta JSON
+            return response.json(); 
           } else {
             throw new Error('Error al buscar el libro.');
           }
         })
         .then(function(data) {
-          resultsContainer.innerHTML = ''; // Limpiar el contenido existente
+          
+          resultsContainer.innerHTML = ''; 
   
           if (data.length > 0) {
-            var ul = document.createElement('ul');
+            let ul = document.createElement('ul');
   
             data.forEach(function(book) {
-              var li = document.createElement('li');
-              var bookDetails = document.createTextNode('Título: ' + book.titulo + ', Autor: ' + book.autor + ', disponibilidad: ' + book.disponibilidad + ', Área: ' + book.area);
+              let li = document.createElement('li');
+              li.setAttribute('data-book-id', book.id);
+              
+              let bookDetails = document.createTextNode('Id: ' + book.id + ', Título: ' + book.title + ', Autor: ' + book.author + ', disponibilidad: ' + book.availability + ', Área: ' + book.area );
               li.appendChild(bookDetails);
-  
-              var updateButton = document.createElement('button');
+
+              
+              let updateButton = document.createElement('button');
               updateButton.textContent = 'Actualizar libro';
-            updateButton.addEventListener('click', function() {
+              updateButton.addEventListener('click', function() {
               showUpdateForm = true;
-              libroIdToUpdate = book.id; // Almacena el ID del libro a actualizar
+              bookIdToUpdate = book.id; 
               renderBooks();
             });
   
-              var deleteButton = document.createElement('button');
+              let deleteButton = document.createElement('button');
               deleteButton.textContent = 'Eliminar libro';
               deleteButton.addEventListener('click', function() {
-                eliminarLibro(book.id);
+                deleteBook(book.id);
               });
   
               li.appendChild(updateButton);
@@ -119,7 +121,7 @@ document.addEventListener('DOMContentLoaded', function() {
   
             resultsContainer.appendChild(ul);
           } else {
-            var noBooksMessage = document.createTextNode('No se encontraron libros.');
+            let noBooksMessage = document.createTextNode('No se encontraron libros.');
             resultsContainer.appendChild(noBooksMessage);
           }
         })
@@ -129,15 +131,20 @@ document.addEventListener('DOMContentLoaded', function() {
         });
   
       // Limpiar los campos del formulario después de realizar la búsqueda
-      document.getElementById('libro-nombre').value = '';
-      document.getElementById('libro-autor').value = '';
+      document.getElementById('book-name').value = '';
+      document.getElementById('book-author').value = '';
     });
-  
-    function eliminarLibro(libroId) {
-      var deleteUrl = 'http://127.0.0.1:4000/api/libros/' + libroId;
+
+    let showUpdateForm = false;
+
+
+    //delete book
+    function deleteBook(bookId) {
+      
+      let deleteUrl = 'https://laupadron.pythonanywhere.com/api/books/' + bookId;
         // Obtener el token de autorización del Local Storage
         const token = localStorage.getItem('token');
-        console.log(token)
+        
       fetch(deleteUrl, {
         method: 'DELETE',
         headers: {
@@ -147,51 +154,61 @@ document.addEventListener('DOMContentLoaded', function() {
       })
         .then(function(response) {
           if (response.ok) {
+            
             return response.json();
           } else {
             throw new Error('Error al eliminar el libro.');
           }
         })
         .then(function(data) {
-          console.log(data);
-          // Mostrar mensaje de éxito o realizar acciones adicionales si es necesario
-        })
+          alert('Libro eliminado correctamente');
+          /// Eliminar visualmente la fila correspondiente en la tabla
+          let listItemToDelete = document.querySelector(`li[data-book-id="${bookId}"]`);
+          
+          if (listItemToDelete) {
+            listItemToDelete.remove();
+          }
+        
+          })
         .catch(function(error) {
           console.log('Error:', error);
           alert('Error al eliminar el libro.');
         });
+        
     }
-    function actualizarLibro(libroId) {
-        var updateUrl = 'http://127.0.0.1:4000/api/libros/' + libroId;
-        const token = localStorage.getItem('token');
-      
-        // Obtener los valores de los campos del formulario
-        var updatedTitulo = document.getElementById('update-titulo').value;
-        var updatedAutor = document.getElementById('update-autor').value;
-        var updatedEditorial = document.getElementById('update-editorial').value;
-        var updatedDispo = document.getElementById('update-disponibilidad').value;
-        var updatedArea = document.getElementById('update-area').value;
-      
-        // Crear un objeto con los datos actualizados del libro
-        var updatedData = {};
+
+    //update book
+    function updateBook(bookId) {
+
+     
+        
+        let updatedTitle = document.getElementById('update-title').value;
+        let updatedAuthor = document.getElementById('update-author').value;
+        let updatedEditorial = document.getElementById('update-editorial').value;
+        let updatedAvaila = document.getElementById('update-availability').value;
+        let updatedArea = document.getElementById('update-area').value;
+        
+        let updatedData = {};
+        
       
         // Verificar y agregar campos opcionales solo si se proporciona un valor
-        if (updatedTitulo.trim() !== '') {
-          updatedData.titulo = updatedTitulo;
+        if (updatedTitle.trim() !== '') {
+          updatedData.title = updatedTitle;
         }
-        if (updatedAutor.trim() !== '') {
-          updatedData.autor = updatedAutor;
+        if (updatedAuthor.trim() !== '') {
+          updatedData.autor = updatedAuthor;
         }
         if (updatedEditorial.trim() !== '') {
           updatedData.editorial = updatedEditorial;
         }
-        if (updatedDispo.trim() !== '') {
-            updatedData.editorial = updatedDispo;
+        if (updatedAvaila.trim() !== '') {
+            updatedData.editorial = updatedAvaila;
           }
         if (updatedArea.trim() !== '') {
           updatedData.area = updatedArea;
         }
-      
+        let updateUrl = 'https://laupadron.pythonanywhere.com/api/books/' + bookId;
+        const token = localStorage.getItem('token');
         fetch(updateUrl, {
           method: 'PUT',
           headers: {
@@ -208,8 +225,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
           })
           .then(function(data) {
-            console.log(data);
-            // Mostrar mensaje de éxito o realizar acciones adicionales si es necesario
+            alert('se actualizó correctamente la base de datos')
           })
           .catch(function(error) {
             console.log('Error:', error);
@@ -219,72 +235,84 @@ document.addEventListener('DOMContentLoaded', function() {
       
       
     function renderBooks() {
+
+      let updateForm = document.createElement('form');
+      updateForm.setAttribute('id', 'update_form');
+
         if (showUpdateForm) {
           resultsContainer.innerHTML = '';
       
-          var updateForm = document.createElement('form');
-      
-          var titleLabel = document.createElement('label');
+          let titleLabel = document.createElement('label');
           titleLabel.textContent = 'Título:';
-          var titleInput = document.createElement('input');
+          let titleInput = document.createElement('input');
           titleInput.setAttribute('type', 'text');
-          titleInput.setAttribute('id', 'update-titulo');
+          titleInput.setAttribute('id', 'update-title');
           updateForm.appendChild(titleLabel);
           updateForm.appendChild(titleInput);
       
-          var authorLabel = document.createElement('label');
+          let authorLabel = document.createElement('label');
           authorLabel.textContent = 'Autor:';
-          var authorInput = document.createElement('input');
+          let authorInput = document.createElement('input');
           authorInput.setAttribute('type', 'text');
-          authorInput.setAttribute('id', 'update-autor');
+          authorInput.setAttribute('id', 'update-author');
           updateForm.appendChild(authorLabel);
           updateForm.appendChild(authorInput);
       
-          var publisherLabel = document.createElement('label');
+          let publisherLabel = document.createElement('label');
           publisherLabel.textContent = 'Editorial:';
-          var publisherInput = document.createElement('input');
+          let publisherInput = document.createElement('input');
           publisherInput.setAttribute('type', 'text');
           publisherInput.setAttribute('id', 'update-editorial');
           updateForm.appendChild(publisherLabel);
           updateForm.appendChild(publisherInput);
 
-          var dispoLabel = document.createElement('label');
+          let dispoLabel = document.createElement('label');
           dispoLabel.textContent = 'Disponibilidad:';
-          var dispoInput = document.createElement('input');
+          let dispoInput = document.createElement('input');
           dispoInput.setAttribute('type', 'text');
-          dispoInput.setAttribute('id', 'update-disponibilidad');
+          dispoInput.setAttribute('id', 'update-availability');
           updateForm.appendChild(dispoLabel);
           updateForm.appendChild(dispoInput);
       
-          var areaLabel = document.createElement('label');
+          let areaLabel = document.createElement('label');
           areaLabel.textContent = 'Área:';
-          var areaInput = document.createElement('input');
+          let areaInput = document.createElement('input');
           areaInput.setAttribute('type', 'text');
           areaInput.setAttribute('id', 'update-area');
           updateForm.appendChild(areaLabel);
           updateForm.appendChild(areaInput);
       
-          var submitButton = document.createElement('button');
+          let submitButton = document.createElement('button');
           submitButton.textContent = 'Actualizar';
           updateForm.appendChild(submitButton);
+
+          let deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Eliminar libro';
+        deleteButton.addEventListener('click', function() {
+        deleteBook(bookIdToUpdate);
+          });
+
+        updateForm.appendChild(deleteButton);
       
           updateForm.addEventListener('submit', function(event) {
             event.preventDefault();
       
-            var updatedData = {
-              titulo: titleInput.value,
-              autor: authorInput.value,
+            let updatedData = {
+              title: titleInput.value,
+              author: authorInput.value,
               editorial: publisherInput.value,
-              disponibilidad: dispoInput.value,
+              availability: dispoInput.value,
               area: areaInput.value
             };
       
-            actualizarLibro(libroIdToUpdate, updatedData);
+            updateBook(bookIdToUpdate, updatedData);
           });
+          
+          
       
           resultsContainer.appendChild(updateForm);
         } else {
-          // Resto del código para mostrar los libros y los botones de actualización y eliminación
+          
         }
       }
       
--- library_consult/Administrator/login_admin.js
+++ library_consult/Administrator/login_admin.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Obtener el formulario de inicio de sesión por su id
     const loginForm = document.getElementById('loginForm');
   
     // Agregar un evento de envío al formulario
@@ -7,11 +6,11 @@ document.addEventListener('DOMContentLoaded', function() {
       event.preventDefault(); // Evitar que el formulario se envíe automáticamente
   
       // Obtener los valores de los campos de entrada
-      const name = document.getElementById('username').value;
-      const password = document.getElementById('password').value;
+      let name = document.getElementById('username').value;
+      let password = document.getElementById('password').value;
   
       // Realizar una solicitud POST a la API de inicio de sesión
-      fetch('http://127.0.0.1:4000/api/login', {
+      fetch('https://laupadron.pythonanywhere.com/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -24,18 +23,15 @@ document.addEventListener('DOMContentLoaded', function() {
       .then(response => response.json())
       .then(data => {
         
-        
-        // Verificar la respuesta de la API
+        // Verificamos la respuesta de la API
         if (data.message === 'Inicio de sesión exitoso') {
-          // Almacenar el token en el Local Storage
+          // Almacenamos el token en el Local Storage
           localStorage.setItem('token', data.token);
           console.log(localStorage.getItem('token'))
-          // Redireccionar al formulario de agregar libros
-          
-          window.location.replace('./add_delete_book.html'); // Reemplaza '/agregar-libros' con la ruta deseada
-
+          // Redireccionamos al formulario de agregar libros
+          window.location.replace('./add_delete_book.html'); 
         } else {
-          // Redireccionar a la página anterior (inicio de sesión fallido)
+          
           window.location.href = '../library_consult.html'
           
         }
